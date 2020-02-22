f43d43db1a8c775d384afb57fdea46157c960898
Евгений Сергеев <evgeniisergeev@MacBook-Pro-Evgenii.local>
2020-02-22 22:53:22 +0300

    Lesson7

---
+++ lesson7/project_express/server/cart.js
@@ -0,0 +1,22 @@
+const add = (cart, req) => {
+    cart.contents.push(req.body);
+    return JSON.stringify(cart, null, 4);
+};
+
+const change = (cart, req) => {
+    const find = cart.contents.find(el => el.id_product === +req.params.id);
+    find.quantity += req.body.quantity;
+    return JSON.stringify(cart, null, 4);
+};
+
+const remove = (cart, req) => {
+    const find = cart.contents.find(el => el.id_product === +req.params.id);
+    cart.contents.splice(cart.contents.indexOf(req.body));
+    return JSON.stringify(cart, null, 4);
+};
+
+module.exports = {
+    add,
+    change,
+    remove,
+};
